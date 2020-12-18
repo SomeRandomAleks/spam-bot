b9be84a7b201f5f789fa7c7fc9219c9818247ddb
SomeRandomAleks <74792495+SomeRandomAleks@users.noreply.github.com>
2020-12-18 18:18:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 time.sleep(3)
-f = open("Bee Movie Script - Dialogue Transcript", 'r')
+f = open("text", 'r')
 for word in f:
 	pyautogui.typewrite(word)
 	pyautogui.press("enter")
